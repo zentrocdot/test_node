f45538bd0e59b3b5aa9f033bfb254429fa994f27
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-03 17:47:34 +0100

    Update nodes.py

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -273,71 +273,6 @@ def updateTextWidget(node, widget, text):
     # version of ComfyUI.
     PromptServer.instance.send_sync("exectails.text_updater.node_processed", {"node": node, "widget": widget, "text": text})
 
-
-class ETTokenCountNode:
-    """
-    A node that counts the number of tokens in a given text. Passes the
-    text through and returns the token count.
-    """
-
-    @classmethod
-    def INPUT_TYPES(cls):
-        return {
-            "required": {
-                "text": ("STRING", {"forceInput": True}),
-                "clip": ("CLIP",),
-                "tokens": ("STRING", {"default": "", "multiline": False}),
-            },
-            "hidden": {
-                "unique_id": "UNIQUE_ID",
-            },
-        }
-
-    RETURN_TYPES = ("STRING", "INT",)
-    RETURN_NAMES = ("text", "count")
-    OUTPUT_IS_LIST = (True, False,)
-    INPUT_IS_LIST = True
-    OUTPUT_NODE = True
-
-    CATEGORY = "exectails/Info"
-    FUNCTION = "process"
-
-    def process(self, text, clip, tokens, unique_id):
-        texts = text
-        clip = clip[0]
-
-        tokens_min = 999999999
-        tokens_max = 0
-        tokens_avg = 0
-        tokens_out = ""
-
-        for text in texts:
-            token_count = self.get_token_count(text, clip)
-            tokens_min = min(tokens_min, token_count)
-            tokens_max = max(tokens_max, token_count)
-            tokens_avg += token_count
-
-        tokens_avg = int(tokens_avg / len(texts))
-
-        if tokens_min == tokens_max:
-            tokens_out = str(tokens_min)
-        else:
-            tokens_out = f"~{tokens_avg} ({tokens_min}-{tokens_max})"
-
-        updateTextWidget(unique_id, "tokens", tokens_out)
-        return {"ui": {"tokens": tokens_out}, "result": (texts, tokens_avg,)}
-
-    def get_token_count(self, text, clip):
-        tokens = clip.tokenize(text)
-        result = 0
-
-        if 'g' in tokens and len(tokens['g']) > 0:
-            real_tokens = [token for token in tokens['g'][0] if token[0] != 0]
-            result = len(real_tokens)
-
-        return result
-
-
 class ETShowDataNode:
     """
     A node that takes any value and displays it as a string.
@@ -388,56 +323,6 @@ class ETShowDataNode:
         return result
 
 
-class ETInspectTextNode:
-    """
-    A node that takes any value, displays it as a string, and passes
-    through on as output.
-    """
 
-    @classmethod
-    def INPUT_TYPES(s):
-        return {
-            "required": {
-                "text": ("STRING", {"forceInput": True}),
-                "display": ("STRING", {"default": "", "multiline": True}),
-            },
-            "hidden": {
-                "unique_id": "UNIQUE_ID",
-            },
-        }
-
-    RETURN_TYPES = ("STRING",)
-    RETURN_NAMES = ("text",)
-    INPUT_IS_LIST = (True,)
-    OUTPUT_IS_LIST = (True,)
-    OUTPUT_NODE = True
-
-    CATEGORY = "exectails/Info"
-    FUNCTION = "process"
-
-    def process(self, text, display, unique_id):
-        displayText = self.render(text)
-
-        updateTextWidget(unique_id, "display", displayText)
-        return {"ui": {"display": displayText}, "result": (text,)}
-
-    def render(self, input):
-        if not isinstance(input, list):
-            return input
-
-        listLen = len(input)
-
-        if listLen == 0:
-            return ""
-
-        if listLen == 1:
-            return input[0]
-
-        result = "List:\n"
-
-        for i, element in enumerate(input):
-            result += f"- {input[i]}\n"
-
-        return result
 
 
