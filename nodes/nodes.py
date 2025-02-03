361b3a7e4afe898ddce23ac1a022cbc7a6d8086e
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-03 13:58:51 +0100

    Update nodes.py

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -19,13 +19,13 @@ def pil2tensor(image):
     # Return tensor.
     return torch.from_numpy(np.array(image).astype(np.float32) / 255.0).unsqueeze(0)
 
-class AnyType(str):
-    '''A special class that is always equal in not equal comparisons. Credit to Rgthree / pythongosssss'''
-
-    def __ne__(self, __value: object) -> bool:
-        return False
-
-any = AnyType("*")
+#class AnyType(str):
+#    '''A special class that is always equal in not equal comparisons. Credit to Rgthree / pythongosssss'''
+#
+#    def __ne__(self, __value: object) -> bool:
+#        return False
+#
+#any = AnyType("*")
 
 class CircleDetection:
     '''Circle detection node.'''
@@ -175,10 +175,10 @@ class DisplayData:
         }
 
     RETURN_TYPES = ()
-    #RETURN_TYPES = ("STRING",)
     FUNCTION = "display_data"
-    #OUTPUT_NODE = True
-
+    OUTPUT_NODE = True
+    CATEGORY = "🧬 Object Detection Nodes"
+    
     def display_data(self, source=None):
         '''Display data.'''
         print("Source: Gotcha!")
@@ -198,3 +198,47 @@ class DisplayData:
         print("Source:", source) 
         value = "test"
         return {"ui": {"text": (value,)}}
+
+class Everything(str):
+    def __ne__(self, _):
+        return False
+
+class ShowText:
+    @classmethod
+    def INPUT_TYPES(cls):
+        return {
+            "required": {
+                "text_int_float": (Everything("*"), {"forceInput": True}),
+            },
+        }
+
+    RETURN_TYPES = ()
+    FUNCTION = "show_text"
+    OUTPUT_NODE = True
+    INPUT_IS_LIST = (True,)
+    CATEGORY = "Bjornulf"
+    
+    def detect_type(self, value):
+        if isinstance(value, int):
+            return 'integer'
+        elif isinstance(value, float):
+            # Check if it has a decimal part
+            if value % 1 == 0:
+                return 'float' if str(value).endswith('.0') else 'integer'
+            return 'float'
+        elif isinstance(value, str):
+            try:
+                float_val = float(value)
+                if '.' in value:
+                    return 'float string'
+                if float_val.is_integer():
+                    return 'integer string'
+                return 'float string'
+            except ValueError:
+                return 'normal string'
+        else:
+            return 'other type'
+
+    def show_text(self, text_int_float):
+        type_info = [f"{value}" for value in text_int_float]
+        return {"ui": {"text": type_info}}
