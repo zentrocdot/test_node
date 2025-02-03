75f9641ebf0e46072f637a9164eeb10d654c6b44
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-03 11:41:16 +0100

    Update nodes.py

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -4,7 +4,7 @@
 import numpy as np
 import cv2
 import torch
-from PIL import Image, ImageDraw, ImageFont
+from PIL import Image, ImageDraw, ImageFont, ImageSequence
 
 # Tensor to PIL function.
 def tensor2pil(image):
@@ -145,6 +145,35 @@ class CircleDetection:
         image_out = pil2tensor(img_output)
         # Return None.
         output_string = "Gotcha!"
-        output_mask = image
+        output_masks = []
+        w, h = None, None
+        w = image.size[0]
+        h = image.size[1]
+        for i in ImageSequence.Iterator(img):
+            i = node_helpers.pillow(ImageOps.exif_transpose, i)
+            if i.mode == 'I':
+                i = i.point(lambda i: i * (1 / 255))
+            image = i.convert("RGB")
+            if len(output_images) == 0:
+                w = image.size[0]
+                h = image.size[1]
+            if image.size[0] != w or image.size[1] != h:
+                continue
+            image = np.array(image).astype(np.float32) / 255.0
+            image = torch.from_numpy(image)[None,]
+            if 'A' in i.getbands():
+                mask = np.array(i.getchannel('A')).astype(np.float32) / 255.0
+                mask = 1. - torch.from_numpy(mask)
+            else:
+                mask = torch.zeros((64,64), dtype=torch.float32, device="cpu")
+            output_images.append(image)
+            output_masks.append(mask.unsqueeze(0))
+        if len(output_images) > 1 and img.format not in excluded_formats:
+            output_image = torch.cat(output_images, dim=0)
+            output_mask = torch.cat(output_masks, dim=0)
+        else:
+            #output_image = output_images[0]
+            output_mask = output_masks[0] 
+        #output_mask = image            
         #return (image_out, output_mask, output_string,)
         return (image_out, output_mask, output_string,)
