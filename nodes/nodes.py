4a41c4249e31603abf95d41b4b54bc8b77e567cb
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-03 15:48:01 +0100

    Update nodes.py

--- nodes/nodes.py
+++ nodes/nodes.py
@@ -55,7 +55,7 @@ class CircleDetection:
 
     #RETURN_TYPES = ("IMAGE", "MASK", "STRING",)
     RETURN_TYPES = ("IMAGE", "IMAGE", "STRING",)
-    RETURN_NAMES = ("IMAGE (Detection)", "IMAGE (Mask), "Data",)
+    #RETURN_NAMES = ("IMAGE (Detection)", "IMAGE (Mask), "Data",)
     FUNCTION = "circle_detection"
     CATEGORY = "🧬 Object Detection Nodes"
     OUTPUT_NODE = True
