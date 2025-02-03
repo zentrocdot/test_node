4d08e520230ca91b7bd8d4b23f3977848b1df642
Dr. Peter Netz <152088022+zentrocdot@users.noreply.github.com>
2025-02-03 13:58:27 +0100

    Update __init__.py

--- __init__.py
+++ __init__.py
@@ -3,6 +3,7 @@ from .nodes.nodes import *
 NODE_CLASS_MAPPINGS = { 
     "Circle Detection": CircleDetection,
     "Display Data": DisplayData,
+    "Show Text": ShowText,
     }
     
 print("\033[34mComfyUI Circle Detection Nodes: \033[92mLoaded\033[0m")
